dd339df45727da73cd8acee48e0ec16890e2d888
Bidisha Roy <30764590+bidisha-br@users.noreply.github.com>
2020-05-22 10:48:07 +0530

    Add files via upload

---
+++ cacheMatrix.R
@@ -0,0 +1,54 @@
+#first function makes a special matrix
+makeCacheMatrix<- function(sp_mat = matrix()){
+  inv <- NULL #null inverse initialization
+  #set t he matrix
+  set<-function(matrix){
+    sp_mat<<- matrix
+    inv <<- NULL
+  }
+  #get the matrix
+  get<- function(){
+    sp_mat #Return the matrix
+  }
+  
+  #set the inverse of the matrix
+  setInverse <- function(inverse){
+    inv <<-inverse
+  }
+  
+  #get the inverse of the matrix
+  getInverse <- function(){
+    inv 
+  }
+  
+  #list of methods declared
+  list(set=set, get=get, setInverse=setInverse, getInverse=getInverse)
+}
+
+#compute special inverse 
+
+cacheSolve<- function(x, ...){
+  
+  #return inverse of matrix x
+  sp_mat<- x$getInverse()
+  
+  #if inverse is already set return
+  if( !is.null(sp_mat) ) {
+    message("getting cached data")
+    return(sp_mat)
+  }
+  
+  ##get the matrix from the object x
+  
+  data<- x$get()
+  
+  ## Calculate the inverse using usual matrix multiplication
+  sp_mat <- solve(data) %*% data
+  
+  ## Set the inverse to the object
+  x$setInverse(sp_mat)
+  
+  ## Return the matrix
+  sp_mat
+  
+}
